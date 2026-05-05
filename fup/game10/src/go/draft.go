95fddfccaee2242597529b9cef26a8917468236a
3MiLLy <emi.llyfernandes.almeida0@gmail.com>
2026-05-05 18:23:05 +0000

    ainda 05/05

---
+++ fup/game10/src/go/draft.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+func main() {
+    var n, d, a int64
+
+    fmt.Scan(&n, &d, &a)
+
+    
+}
---
+++ fup/jokenpo/src/go/jokenpo.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+    var jog1, jog2 rune
+
+    fmt.Scan(&jog1, &jog2)
+
+    if jog1=='R' && jog2=='S' {
+        fmt.Println("jog1")
+    } else if jog1=='P' && jog2=='R'{
+        fmt.Println("jog1")
+    } else if jog1=='S' && jog2=='P'{
+        fmt.Println("jog1")
+    }
+
+}
