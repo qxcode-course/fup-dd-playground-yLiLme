b3eb9ef7fe79ad888f66f09c70528b07aabc9e50
Emilly Victoria <emi.llyfernandes.almeida0@gmail.com>
2026-07-02 20:10:49 -0300

    sei nao

--- fup/leds/src/go/draft.go
+++ fup/leds/src/go/draft.go
@@ -1,5 +1,27 @@
 package main
-import "fmt"
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"text/scanner"
+)
 func main() {
-    
+	var n, tl int
+	fmt.Scan(&n)
+
+	scanner:=bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	for i:=0; i<n; i++{
+		scanner.Scan()
+		numled:=scanner.Text()
+	}
+	for i:=0; i<n; i++{
+		if numled[i]==1{
+			
+		}
+	}
+	
+
 }
--- fup/pedra_struct/src/go/draft.go
+++ fup/pedra_struct/src/go/draft.go
@@ -1,5 +1,57 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
+type Jogadas struct {
+	a, b int
+}
+
+func calcPontos(jogada Jogadas) (bool, int) {
+	if jogada.a < 10 || jogada.b < 10 {
+		return false, 0
+	}
+
+	var d int
+	if jogada.a > jogada.b {
+		d = jogada.a - jogada.b
+	} else {
+		d = jogada.b - jogada.a
+	}
+	return true, d
+}
+
+func BestJogada(jogada []Jogadas) int {
+	b := -1
+	mP := 99999
+
+	for i := 0; i < len(jogada); i++ {
+		v, p := calcPontos(jogada[i])
+
+		if v && p < mP {
+			mP = p
+			b = i
+		}
+	}
+	return b
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	var n int
+	fmt.Scan(&n)
+
+	jogs := make([]Jogadas, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&jogs[i].a, &jogs[i].b)
+	}
+
+	result := BestJogada(jogs)
+
+	if result == -1 {
+		fmt.Println("sem ganhador")
+	} else {
+		fmt.Println(result)
+	}
 }
